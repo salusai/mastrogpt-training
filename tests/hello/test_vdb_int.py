308990ad0e87aa2f114ac5e7c320777a7780c1e9
Michele Sciabarra <msciabarra@apache.org>
2025-01-20 11:22:13 +0100

    test vbd int again

--- tests/hello/test_vdb_int.py
+++ tests/hello/test_vdb_int.py
@@ -5,30 +5,30 @@ def test_vdb_int():
     args = {"drop_collection": "test"}
     res = req.post(vdb, json=args).json()
     assert res.get("output").startswith("Start with '*'")
-    
 
     args = {"input": "Hello, world."}
     res = req.post(vdb, json=args).json()
+    res.get("output").startswith("OK:")
+    
     args = {"input": "Hello again, world."}
     req.post(vdb, json=args).json()
+    res.get("output").startswith("OK:")
+    
     args = {"input": "We are on earth."}
     req.post(vdb, json=args).json()
-
-    args = {"input": "*world"}
-    # three times for a timing issue
-    req.post(vdb, json=args)
-    req.post(vdb, json=args)
-    res = req.post(vdb, json=args).json()
+    res.get("output").startswith("OK:")
     
-    assert res.get("output").count("world") >= 2
+    args = {"input": "*world"}
+    # retry a few tims for timing issues
+    for i in range(0, 10):
+        out = req.post(vdb, json=args).json().get("output")
+        if out.startswith("Found"):
+            print(f"Found at {i}")
+            break
+        
+    assert out.count("world") >= 2
     
     args = {"input": "!world"}
-    req.post(vdb, json=args).json()
-
-    args = {"input": "*world"}
-    req.post(vdb, json=args)
-    req.post(vdb, json=args)
     res = req.post(vdb, json=args).json()
-    assert res.get("output").count("world") == 0
-    
-    
+    assert res.get("output").find("Deleted: 2") != -1
+        
