84fba066798b5d267b4b5e17f94f99fcd39bfb3e
Michele Sciabarra <msciabarra@apache.org>
2025-02-13 22:07:40 +0100

    puzzle

--- packages/form/postgen/postgen.py
+++ packages/form/postgen/postgen.py
@@ -1,5 +1,7 @@
-USAGE = """I will generate a post on Apache OpenServerless post for you! 
-Tell me 'post' and I will ask for the required informations."""
+MODEL = "llama3.1:8b"
+STREAMING = True
+
+USAGE = """I will generate a post on Apache OpenServerless post for you!"""
 
 FORM = [
   {
@@ -15,54 +17,80 @@ FORM = [
     "required": "true"
   },
   {
-      "name": "tone",
-      "label": "What tone should the post have?",
-      "type": "text",
-      "required": "true"
+    "name": "tone",
+    "label": "What tone should the post have?",
+    "type": "text",
+    "required": "true"
   }
 ]
 
-
-MODEL = "llama3.1:8b"
+import json, socket, traceback
+def stream(args, lines):
+  
+  if type(lines) is str:
+    ls = [{"response":x+" "} for x in lines.split(" ")]
+    lines = (json.dumps(x).encode('utf-8') for x in ls)
+  
+  sock = args.get("STREAM_HOST")
+  port = int(args.get("STREAM_PORT"))
+  out = ""
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((sock, port))
+    try:
+      for line in lines:
+        print(line, end='')
+        dec = json.loads(line.decode("utf-8")).get("response")
+        msg = {"output": dec }
+        out += dec
+        s.sendall(json.dumps(msg).encode("utf-8"))
+    except Exception as e:
+      traceback.print_exc(e)
+      out = str(e)
+  return out
 
 import os, requests as req
 def chat(args, inp):
   host = args.get("OLLAMA_HOST", os.getenv("OLLAMA_HOST"))
   auth = args.get("AUTH", os.getenv("AUTH"))
   url = f"https://{auth}@{host}/api/generate"
-  msg = { "model": MODEL, "prompt": inp, "stream": False}
-  res = req.post(url, json=msg).json()
-  return  res.get("response", "error")
+  msg = { "model": MODEL, "prompt": inp, "stream": STREAMING}
+  if STREAMING:
+    lines = req.post(url, json=msg, stream=True).iter_lines()
+    out = stream(args, lines)
+  else:
+    res = req.post(url, json=msg).json()
+    out = res.get("response", "error")
+  
+  return  out
  
-
 def postgen(args):
-  print(args)
+  #print(args)
 
-  res = {}
+  res = {"streaming": STREAMING}
   inp = args.get("input", "")
   out = USAGE
 
-  print("TYPE", type(inp))
+  #print("TYPE", type(inp))
 
-  if type(inp) is dict and "form" in inp:
+  if inp == "":
+     out = USAGE
+     res['form'] = FORM
+
+  elif type(inp) is dict and "form" in inp:
       data = inp["form"]
-      inp = f"""Generate a post promoting Apache OpenServerless.
-Your job role is {data['job']}.
-The reason because you are Apache OpenServerless is {data['why']}.
-The tone of the post should be {data['tone']}.
-"""
-      out = chat(args, inp)
       for field in data.keys():
         print(data[field])
 
-  elif inp == "post":
-    out = "please fill the form"
-    res['form'] = FORM
+      inp = f"""Generate a post promoting Apache OpenServerless.
+Your job role is {data['job']}.
+The reason bfecause you are Apache OpenServerless is {data['why']}.
+The tone of the post should be {data['tone']}.
+"""
+      out = chat(args, inp)
+
   elif inp != "":
     out = chat(args, inp)
 
   res['output'] = out
+  print(res)
   return res
-
-
-
