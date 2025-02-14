ddf369e1bf57ffd1483c1be05ef2ff157ed9682e
Michele Sciabarra <msciabarra@apache.org>
2025-02-14 17:12:28 +0100

    cleanup

--- packages/packages/assistant/assistant/__main__.py
+++
@@ -1,5 +0,0 @@
-#--kind python:default
-#--web true
-import assistant
-def main(args):
-  return { "body": assistant.assistant(args) }
--- packages/packages/assistant/assistant/assistant.py
+++
@@ -1,2 +0,0 @@
-def assistant(args):
-  return { "output": "assistant" }
