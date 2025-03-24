86a2dd133fea9cb34987261217dd7a4ef244bf00
salusai <129303409+salusai@users.noreply.github.com>
2025-03-24 13:41:21 +0000

    updated vision

--- packages/vision/form/form.py
+++ packages/vision/form/form.py
@@ -1,5 +1,8 @@
 import os, requests as req
+import base64, pathlib, boto3
 import vision
+from vision.store.bucket import Bucket
+from datetime import datetime
 
 USAGE = "Please upload a picture and I will tell you what I see"
 FORM = [
@@ -19,9 +22,19 @@ def form(args):
   if type(inp) is dict and "form" in inp:
     img = inp.get("form", {}).get("pic", "")
     print(f"uploaded size {len(img)}")
+    # decode image
     vis = vision.Vision(args)
     out = vis.decode(img)
-    res['html'] = f'<img src="data:image/png;base64,{img}">'
+    # upload to S3
+    ## setup the bucket
+    s3bucket = Bucket(args)
+    ## define image key and write the file
+    image_key = "file_" + datetime.now().strftime("%Y%m%d_%H%M%S%f")+".jpg"
+    s3bucket.write(image_key, out)
+    # read the file from s3
+    img_url = s3bucket.exturl(image_key, 3600)
+    # return external link to image
+    res['html'] = f'<img src="{img_url}">'
     
   res['form'] = FORM
   res['output'] = out
