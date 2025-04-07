2b42ecffb4be84fcd91454b29aa7d2568e29aae2
salusai <129303409+salusai@users.noreply.github.com>
2025-04-07 21:33:52 +0000

    susai lesson 6

--- packages/vdb/load/load.py
+++ packages/vdb/load/load.py
@@ -1,4 +1,7 @@
 import vdb
+import re
+import requests as req
+from bs4 import BeautifulSoup
 
 USAGE = f"""Welcome to the Vector DB Loader.
 Write text to insert in the DB.
@@ -27,10 +30,43 @@ def load(args):
   elif inp.startswith("!"):
     count = db.remove_by_substring(inp[1:])
     out = f"Deleted {count} records."
+  elif inp.startswith("https://"):
+    html = req.get(inp).text
+    soup = BeautifulSoup(html, 'html.parser')
+    page_text = soup.get_text()
+    res = db.insert(page_text)
+    out = "Inserted " 
+    out += " ".join([str(x) for x in res.get("ids", [])])
   elif inp != '':
     res = db.insert(inp)
     out = "Inserted " 
     out += " ".join([str(x) for x in res.get("ids", [])])
 
   return {"output": out}
-  
+
+def tokenize(text):
+    tokens = text.split()
+    
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+        if re.match(r'\d{1,2}[/-]\d{1,2}[/-]\d{4}', token):
+            pass
+        elif re.match(r"\w+'s", token):
+            token = re.sub(r"(\w+)'s", r"\1 's", token)
+        elif re.match(r"\w+'\w+", token):
+            token = token.replace("'", "")
+        elif re.match(r"\w+-\w+", token):
+            pass
+        elif re.match(r"\d+(,\d+)*", token):
+            pass
+        else:
+            token = re.sub(r"([^\w\s]+)", r" \1 ", token)
+        token = re.sub(r"(\w+)\.", r"\1", token)
+        token = re.sub(r"(\w+),", r"\1", token)
+        token = re.sub(r"U\.S\.A\.", r"U.S.A.", token)
+        tokens[i] = token
+        i += 1
+    
+    return tokens
+
